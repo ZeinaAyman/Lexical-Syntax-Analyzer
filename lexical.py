1c8d0784514beccb2eeff44d4bf015a03f13a588
zeina <zeina.ayman@hotmail.com>
2022-05-20 19:24:03 +0200

    added identifer, operator, int and double number

--- lexical.py
+++ lexical.py
@@ -1,21 +1,41 @@
 from pydoc import isdata
+from tabulate import tabulate
 import re
 
 
 
 class Token:
-    
-    def isDatatype(self, tokens, input):
+
+    def tokenizer(self, tokens, input):
         for word in input:
+            #tokenizer will look for datatype
             if word in ['int','double','string']:
                 tokens.append(['DATATYPE', word])
-        return tokens
 
+            #tokenizer will look for identifier type
+            elif re.match("^[A-Za-z_][A-Za-z0-9_]*$", word):
+                tokens.append(['IDENTIFIER', word])
+
+            #tokenizer will look for operators
+            elif word in '*-/+%=':
+                tokens.append(['OPERATOR', word])
+
+            # tokenizer will look for integer items and cast them as a integer number
+            elif re.match("^[1-9]\d*$", word):
+                tokens.append(["INTEGER NUMBER", word])
+
+            # tokenizer will look for double items and cast them as a double number
+            elif re.match("^[0-9]+\\.?[0-9]*$", word):
+                tokens.append(["DOUBLE NUMBER", word])
+
+        
+
+        return tokens
 
 
 if __name__ == '__main__':
 
     tokens=[]
-    input =' int x = 5'.split()
+    input =' int x1 = 5.32 + 7'.split()
     tok = Token()
-    print(tok.isDatatype(tokens, input))
+    print(tabulate(tok.tokenizer(tokens, input), tablefmt="grid"))
