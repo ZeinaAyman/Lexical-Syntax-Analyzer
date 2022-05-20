7383d5e112e467f3e1113d237f1b4e03d9d52683
zeina <zeina.ayman@hotmail.com>
2022-05-20 17:37:59 +0200

    oop changes

--- lexical.py
+++ lexical.py
@@ -2,17 +2,20 @@ from pydoc import isdata
 import re
 
 
-tokens=[]
-input =' int x = 5'.split()
+
+class Token:
+    
+    def isDatatype(self, tokens, input):
+        for word in input:
+            if word in ['int','double','string']:
+                tokens.append(['DATATYPE', word])
+        return tokens
 
 
-
-def isDatatype():
-    for word in input:
-        if word in ['int','double','string']:
-            tokens.append(['DATATYPE', word])
-    return tokens
 
 if __name__ == '__main__':
 
-    print(isDatatype())
+    tokens=[]
+    input =' int x = 5'.split()
+    tok = Token()
+    print(tok.isDatatype(tokens, input))
